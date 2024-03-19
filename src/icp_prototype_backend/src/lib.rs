1d14af3ba31d19346e602b41a28f8aab008383a2
Agustinus Theodorus <agustinustheoo@gmail.com>
2024-03-20 04:29:58 +0700

    feat: add timers for querying blocks

--- src/icp_prototype_backend/src/lib.rs
+++ src/icp_prototype_backend/src/lib.rs
@@ -1,4 +1,90 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use candid::{CandidType, Deserialize, Principal};
+use ic_cdk::api::call::CallResult;
+use ic_cdk_macros::*;
+use serde::Serialize;
+use std::cell::RefCell;
+
+mod types;
+
+use types::Response;
+
+thread_local! {
+    static INTERVAL_IN_SECONDS: RefCell<u64> = RefCell::default();
+    static TIMERS: RefCell<ic_cdk_timers::TimerId> = RefCell::default();
 }
+
+#[derive(CandidType, Deserialize, Serialize)]
+struct Error {
+    message: String,
+}
+
+#[derive(CandidType, Deserialize, Serialize, Clone)]
+struct QueryBlocksQueryRequest {
+    start: u64,
+    length: u64,
+}
+
+const LEDGER_CANISTER_ID: &str = "ryjl3-tyaaa-aaaaa-aaaba-cai";
+
+async fn call_query_blocks() {
+    let ledger_principal = Principal::from_text(LEDGER_CANISTER_ID).expect("Invalid principal");
+    let req = QueryBlocksQueryRequest {
+        start: 0,
+        length: 100,
+    };
+    let call_result: CallResult<(Response,)> =
+        ic_cdk::call(ledger_principal, "query_blocks", (req,)).await;
+
+    let _ = call_result.map_err(|e| {
+        ic_cdk::println!("An error occurred: {:?}", e);
+    });
+}
+
+#[ic_cdk::init]
+async fn init() {
+    let seconds = 15;
+    INTERVAL_IN_SECONDS.with(|interval_ref| {
+        interval_ref.replace(seconds);
+    });
+
+    let interval = std::time::Duration::from_secs(seconds);
+    ic_cdk::println!("Starting a periodic task with interval {:?}", interval);
+    let timer_id = ic_cdk_timers::set_timer_interval(interval, || {
+        ic_cdk::spawn(call_query_blocks());
+    });
+
+    TIMERS.with(|timers_ref| {
+        timers_ref.replace(timer_id);
+    });
+}
+
+#[query]
+fn get_interval() -> Result<u64, Error> {
+    INTERVAL_IN_SECONDS.with(|interval_ref| Ok(interval_ref.borrow().clone()))
+}
+
+#[update]
+fn set_interval(seconds: u64) -> Result<u64, Error> {
+    TIMERS.with(|timers_ref| {
+        let timer_id = timers_ref.borrow().clone();
+        ic_cdk_timers::clear_timer(timer_id);
+    });
+
+    let interval = std::time::Duration::from_secs(seconds);
+    ic_cdk::println!("Starting a periodic task with interval {:?}", interval);
+    let new_timer_id = ic_cdk_timers::set_timer_interval(interval, || {
+        ic_cdk::spawn(call_query_blocks());
+    });
+    TIMERS.with(|timers_ref| {
+        timers_ref.replace(new_timer_id);
+    });
+
+    INTERVAL_IN_SECONDS.with(|seconds_ref| {
+        seconds_ref.replace(seconds);
+    });
+
+    Ok(seconds)
+}
+
+// Enable Candid export
+ic_cdk::export_candid!();
---
+++ src/icp_prototype_backend/src/types.rs
@@ -0,0 +1,103 @@
+use candid::{CandidType, Deserialize};
+use serde::Serialize;
+use std::collections::HashMap;
+
+#[derive(CandidType, Deserialize, Serialize, Debug, Clone, PartialEq)]
+pub struct Response {
+    pub certificate: Option<Vec<u8>>,
+    pub blocks: Vec<Block>,
+    pub chain_length: u64,
+    pub first_block_index: u64,
+    pub archived_blocks: Vec<ArchivedBlock>,
+}
+
+#[derive(CandidType, Deserialize, Serialize, Debug, Clone, PartialEq)]
+pub struct Block {
+    pub transaction: Transaction,
+    pub timestamp: Timestamp,
+    pub parent_hash: Option<Vec<u8>>,
+}
+
+#[derive(CandidType, Deserialize, Serialize, Debug, Clone, PartialEq)]
+pub struct Transaction {
+    pub memo: u64,
+    pub icrc1_memo: Option<Vec<u8>>,
+    pub operation: Option<Operation>,
+    pub created_at_time: Timestamp,
+}
+
+#[derive(CandidType, Deserialize, Serialize, Debug, Clone, PartialEq)]
+pub struct Timestamp {
+    pub timestamp_nanos: u64,
+}
+
+#[derive(CandidType, Deserialize, Serialize, Debug, Clone, PartialEq)]
+pub enum Operation {
+    Approve(Approve),
+    Burn(Burn),
+    Mint(Mint),
+    Transfer(Transfer),
+}
+
+#[derive(CandidType, Deserialize, Serialize, Debug, Clone, PartialEq)]
+pub struct Approve {
+    pub fee: E8s,
+    pub from: Vec<u8>,
+    pub allowance_e8s: i64,
+    pub allowance: E8s,
+    pub expected_allowance: Option<E8s>,
+    pub expires_at: Option<Timestamp>,
+    pub spender: Vec<u8>,
+}
+
+#[derive(CandidType, Deserialize, Serialize, Debug, Clone, PartialEq)]
+pub struct Burn {
+    pub from: Vec<u8>,
+    pub amount: E8s,
+    pub spender: Option<Vec<u8>>,
+}
+
+#[derive(CandidType, Deserialize, Serialize, Debug, Clone, PartialEq)]
+pub struct Mint {
+    pub to: Vec<u8>,
+    pub amount: E8s,
+}
+
+#[derive(CandidType, Deserialize, Serialize, Debug, Clone, PartialEq)]
+pub struct Transfer {
+    pub to: Vec<u8>,
+    pub fee: E8s,
+    pub from: Vec<u8>,
+    pub amount: E8s,
+    pub spender: Option<Vec<u8>>,
+}
+
+#[derive(CandidType, Deserialize, Serialize, Debug, Clone, PartialEq)]
+pub struct E8s {
+    pub e8s: u64,
+}
+
+#[derive(CandidType, Deserialize, Serialize, Debug, Clone, PartialEq)]
+pub struct ArchivedBlock {
+    pub callback: HashMap<String, Callback>,
+    pub start: u64,
+    pub length: u64,
+}
+
+#[derive(CandidType, Deserialize, Serialize, Debug, Clone, PartialEq)]
+pub enum Callback {
+    Ok { blocks: Vec<Block> },
+    Err(CallbackError),
+}
+
+#[derive(CandidType, Deserialize, Serialize, Debug, Clone, PartialEq)]
+pub enum CallbackError {
+    BadFirstBlockIndex {
+        requested_index: u64,
+        first_valid_index: u64,
+    },
+    Other {
+        error_message: String,
+        error_code: u64,
+    },
+}
