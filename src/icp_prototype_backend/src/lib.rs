08883d0ee7690198b996e8cc483973aea3555fe4
Agustinus Theodorus <agustinustheoo@gmail.com>
2024-03-27 06:02:09 +0700

    feat: make subaccount search using hashmap and transaction recording

--- src/icp_prototype_backend/src/lib.rs
+++ src/icp_prototype_backend/src/lib.rs
@@ -1,17 +1,19 @@
-use candid::{CandidType, Deserialize};
+use candid::{CandidType, Deserialize, Principal};
 use ic_cdk::api::call::CallResult;
 use ic_cdk_macros::*;
 use serde::Serialize;
 use std::cell::RefCell;
+use std::collections::{hash_map::DefaultHasher, HashMap};
+use std::hash::{Hash, Hasher};
 
 mod memory;
 mod types;
 
-use memory::{INTERVAL_IN_SECONDS, PRINCIPAL};
-use types::{QueryBlocksQueryRequest, Response};
+use memory::{INTERVAL_IN_SECONDS, LAST_BLOCK, PRINCIPAL, TRANSACTIONS};
+use types::{Operation, QueryBlocksQueryRequest, Response, StoredPrincipal, StoredTransactions};
 
 thread_local! {
-    static LIST_OF_SUBACCOUNTS: RefCell<Vec<AccountIdentifier>> = RefCell::default();
+    static LIST_OF_SUBACCOUNTS: RefCell<HashMap<u64, AccountIdentifier>> = RefCell::default();
     static TIMERS: RefCell<ic_cdk_timers::TimerId> = RefCell::default();
 }
 
@@ -25,26 +27,112 @@ struct AccountIdentifier {
     hash: [u8; 28],
 }
 
+fn hash_to_u64(hash: &[u8; 28]) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    hash.hash(&mut hasher);
+    hasher.finish()
+}
+
+fn includes_hash(vec_to_check: &Vec<u8>) -> bool {
+    if vec_to_check.len() != 28 {
+        return false;
+    }
+
+    let hash_slice = vec_to_check.as_slice();
+    let hash_to_check: &[u8; 28] = match hash_slice.try_into() {
+        Ok(arr) => arr,
+        Err(_) => return false,
+    };
+
+    let hash_key = hash_to_u64(hash_to_check);
+    LIST_OF_SUBACCOUNTS.with(|subaccounts| subaccounts.borrow().contains_key(&hash_key))
+}
+
 async fn call_query_blocks() {
     let ledger_principal = PRINCIPAL.with(|stored_ref| stored_ref.borrow().get().clone());
 
-    if ledger_principal.get_principal().is_none() {
-        ic_cdk::println!("Principal is not set");
-        return;
-    }
+    let last_block = LAST_BLOCK.with(|last_block_ref| last_block_ref.borrow().get().clone());
 
-    let ledger_principal = ledger_principal.get_principal().unwrap();
+    let ledger_principal = match ledger_principal.get_principal() {
+        Some(result) => result,
+        None => {
+            ic_cdk::println!("Principal is not set");
+            return;
+        }
+    };
 
     let req = QueryBlocksQueryRequest {
-        start: 0,
+        start: last_block,
         length: 100,
     };
     let call_result: CallResult<(Response,)> =
         ic_cdk::call(ledger_principal, "query_blocks", (req,)).await;
 
-    let _ = call_result.map_err(|e| {
-        ic_cdk::println!("An error occurred: {:?}", e);
+    let response = match call_result {
+        Ok((response,)) => response,
+        Err(_) => {
+            ic_cdk::println!("An error occurred");
+            return;
+        }
+    };
+
+    let mut block_count = last_block;
+    response.blocks.iter().for_each(|block| {
+        block.transaction.operation.as_ref().map(|operation| {
+            let subaccount_exist = match operation {
+                Operation::Approve(data) => {
+                    let from = data.from.clone();
+                    if includes_hash(&from) {
+                        true
+                    } else {
+                        let spender = data.spender.clone();
+                        includes_hash(&spender)
+                    }
+                }
+                Operation::Burn(data) => {
+                    let from = data.from.clone();
+                    if includes_hash(&from) {
+                        true
+                    } else {
+                        match &data.spender {
+                            Some(spender) => includes_hash(&spender),
+                            None => false,
+                        }
+                    }
+                }
+                Operation::Mint(data) => {
+                    let to = data.to.clone();
+                    includes_hash(&to)
+                }
+                Operation::Transfer(data) => {
+                    let from = data.from.clone();
+                    let to = data.to.clone();
+                    if includes_hash(&from) {
+                        true
+                    } else if includes_hash(&to) {
+                        true
+                    } else {
+                        match &data.spender {
+                            Some(spender) => includes_hash(&spender),
+                            None => false,
+                        }
+                    }
+                }
+            };
+
+            if subaccount_exist {
+                TRANSACTIONS.with(|transactions_ref| {
+                    let transactions = transactions_ref.borrow_mut();
+                    let transaction =
+                        StoredTransactions::new(block_count, block.transaction.clone());
+                    transactions.set(block_count, &transaction);
+                });
+            }
+        });
+        block_count += 1;
     });
+
+    let _ = LAST_BLOCK.with(|last_block_ref| last_block_ref.borrow_mut().set(block_count));
 }
 
 #[ic_cdk::init]
