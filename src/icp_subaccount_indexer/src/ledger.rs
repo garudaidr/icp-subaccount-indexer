8557ba12d1ba3335ee737d12a357174d78659763
Theo <agustinustheoo@gmail.com>
2025-04-15 09:57:01 +0800

    refactor: with cargo clippy

--- src/icp_subaccount_indexer/src/ledger.rs
+++ src/icp_subaccount_indexer/src/ledger.rs
@@ -242,7 +242,7 @@ impl LedgerTransaction for Transaction {
 
     fn generate_hash(&self) -> HashOf<Self> {
         let mut state = Sha256::new();
-        state.update(&serde_cbor::ser::to_vec_packed(&self).unwrap());
+        state.update(serde_cbor::ser::to_vec_packed(&self).unwrap());
         let result = state.finalize();
         let fixed_result: [u8; HASH_LENGTH] = result.into();
         HashOf::new(fixed_result)
