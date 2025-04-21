0b1a223be7ddc46102253869529d4eeadda689a3
Theo <agustinustheoo@gmail.com>
2025-04-21 16:17:03 +0700

    feat: add multi-token support and ICRC account conversion functionality

--- src/icp_subaccount_indexer/icp_subaccount_indexer.did.ts
+++ src/icp_subaccount_indexer/icp_subaccount_indexer.did.ts
@@ -22,6 +22,15 @@ export interface E8s {
 export interface Error {
   message: string;
 }
+export interface HttpHeader {
+  value: string;
+  name: string;
+}
+export interface HttpResponse {
+  status: bigint;
+  body: Uint8Array | number[];
+  headers: Array<HttpHeader>;
+}
 export interface Mint {
   to: Uint8Array | number[];
   amount: E8s;
@@ -34,22 +43,28 @@ export type Operation =
   | { Transfer: Transfer };
 export type Result = { Ok: string } | { Err: Error };
 export type Result_1 = { Ok: string } | { Err: string };
+export type Result_10 = { Ok: null } | { Err: Error };
+export type Result_11 = { Ok: bigint } | { Err: Error };
+export type Result_12 = { Ok: Array<string> } | { Err: Error };
+export type Result_13 = { Ok: boolean } | { Err: Error };
 export type Result_2 = { Ok: Array<StoredTransactions> } | { Err: Error };
 export type Result_3 = { Ok: bigint } | { Err: string };
 export type Result_4 = { Ok: Network } | { Err: string };
 export type Result_5 = { Ok: number } | { Err: string };
 export type Result_6 = { Ok: [] | [bigint] } | { Err: string };
-export type Result_7 = { Ok: Array<StoredTransactions> } | { Err: string };
-export type Result_8 = { Ok: bigint } | { Err: Error };
-export type Result_9 = { Ok: Array<string> } | { Err: Error };
+export type Result_7 = { Ok: Array<[TokenType, string]> } | { Err: string };
+export type Result_8 = { Ok: TokenType } | { Err: string };
+export type Result_9 = { Ok: Array<StoredTransactions> } | { Err: string };
 export interface StoredTransactions {
   sweep_status: SweepStatus;
   memo: bigint;
+  token_ledger_canister_id: Principal;
   icrc1_memo: [] | [Uint8Array | number[]];
   operation: [] | [Operation];
   index: bigint;
   created_at_time: Timestamp;
   tx_hash: string;
+  token_type: TokenType;
 }
 export type SweepStatus =
   | { Swept: null }
@@ -58,6 +73,7 @@ export type SweepStatus =
 export interface Timestamp {
   timestamp_nanos: bigint;
 }
+export type TokenType = { ICP: null } | { CKUSDC: null } | { CKUSDT: null };
 export interface Transfer {
   to: Uint8Array | number[];
   fee: E8s;
@@ -65,28 +81,41 @@ export interface Transfer {
   amount: E8s;
   spender: [] | [Uint8Array | number[]];
 }
+export interface TransformArgs {
+  context: Uint8Array | number[];
+  response: HttpResponse;
+}
 export interface _SERVICE {
   add_subaccount: ActorMethod<[], Result>;
   canister_status: ActorMethod<[], Result_1>;
   clear_transactions: ActorMethod<[[] | [bigint], [] | [Timestamp]], Result_2>;
+  convert_to_icrc_account: ActorMethod<[string], Result>;
   get_canister_principal: ActorMethod<[], Result_1>;
+  get_icrc_account: ActorMethod<[number], Result>;
   get_interval: ActorMethod<[], Result_3>;
   get_network: ActorMethod<[], Result_4>;
   get_next_block: ActorMethod<[], Result_3>;
   get_nonce: ActorMethod<[], Result_5>;
   get_oldest_block: ActorMethod<[], Result_6>;
+  get_registered_tokens: ActorMethod<[], Result_7>;
   get_subaccount_count: ActorMethod<[], Result_5>;
   get_subaccountid: ActorMethod<[number], Result>;
+  get_transaction_token_type: ActorMethod<[string], Result_8>;
   get_transactions_count: ActorMethod<[], Result_5>;
   get_webhook_url: ActorMethod<[], Result_1>;
-  list_transactions: ActorMethod<[[] | [bigint]], Result_7>;
+  list_transactions: ActorMethod<[[] | [bigint]], Result_9>;
   refund: ActorMethod<[bigint], Result>;
-  set_interval: ActorMethod<[bigint], Result_8>;
-  set_next_block: ActorMethod<[bigint], Result_8>;
-  set_sweep_failed: ActorMethod<[string], Result_9>;
+  register_token: ActorMethod<[TokenType, string], Result_10>;
+  set_interval: ActorMethod<[bigint], Result_11>;
+  set_next_block: ActorMethod<[bigint], Result_11>;
+  set_sweep_failed: ActorMethod<[string], Result_12>;
   set_webhook_url: ActorMethod<[string], Result>;
-  single_sweep: ActorMethod<[string], Result_9>;
-  sweep: ActorMethod<[], Result_9>;
+  single_sweep: ActorMethod<[string], Result_12>;
+  sweep: ActorMethod<[], Result_12>;
+  sweep_by_token_type: ActorMethod<[TokenType], Result_12>;
+  sweep_subaccount: ActorMethod<[string, number, TokenType], Result_11>;
+  transform: ActorMethod<[TransformArgs], HttpResponse>;
+  validate_icrc_account: ActorMethod<[string], Result_13>;
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
