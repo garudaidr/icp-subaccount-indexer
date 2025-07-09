7469ba0d70f95a0f25148ca4629a35c0c3cd6efa
Theo <agustinustheoo@gmail.com>
2025-07-10 02:12:20 +0700

    feat(converter): add ckBTC principal support to canister ID converter

--- src/principal_converter/src/canister_id_to_bytes.rs
+++ src/principal_converter/src/canister_id_to_bytes.rs
@@ -2,7 +2,11 @@ use candid::Principal;
 
 fn main() {
     // Convert text IDs to byte slices for use in constants
-    let text_ids = ["xevnm-gaaaa-aaaar-qafnq-cai", "cngnf-vqaaa-aaaar-qag4q-cai"];
+    let text_ids = [
+        "xevnm-gaaaa-aaaar-qafnq-cai", // ckUSDC
+        "cngnf-vqaaa-aaaar-qag4q-cai", // ckUSDT
+        "mxzaz-hqaaa-aaaar-qaada-cai", // ckBTC
+    ];
 
     for id in text_ids {
         let principal = Principal::from_text(id).unwrap();
